0b4bf5ee259a4722b57cb36371ab5b082f20ff98
Serge Semenov <sergii.semenov@skykick.com>
2016-03-16 11:40:41 -0700

    Fix namespace for the ToAsyncEnumerable extension method

--- IEnumerableExtensions.cs
+++ IEnumerableExtensions.cs
@@ -1,8 +1,29 @@
 ﻿using System.Collections.Async;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace System.Collections
+{
+    /// <summary>
+    /// Converts generic IEnumerable to IAsyncEnumerable
+    /// </summary>
+    public static class IEnumerableExtensions
+    {
+        /// <summary>
+        /// Converts IEnumerable to IAsyncEnumerable 
+        /// </summary>
+        /// <param name="enumerable">The instance of IEnumerable to convert</param>
+        /// <param name="runSynchronously">If True the enumeration will be performed on the same thread, otherwise the MoveNext will be executed on a separate thread with Task.Run method</param>
+        /// <returns>Returns an instance of IAsyncEnumerable implementation</returns>
+        public static IAsyncEnumerable ToAsyncEnumerable(this IEnumerable enumerable, bool runSynchronously = false)
+        {
+            if (enumerable == null)
+                throw new ArgumentNullException(nameof(enumerable));
+            return enumerable as IAsyncEnumerable ?? new AsyncEnumerableWrapper<object>(enumerable.Cast<object>(), runSynchronously);
+        }
+    }
+}
+
+namespace System.Collections.Generic
 {
     /// <summary>
     /// Converts generic IEnumerable to IAsyncEnumerable
@@ -21,19 +42,6 @@ namespace System.Collections
             if (enumerable == null)
                 throw new ArgumentNullException(nameof(enumerable));
             return enumerable as IAsyncEnumerable<T> ?? new AsyncEnumerableWrapper<T>(enumerable, runSynchronously);
-        }
-
-        /// <summary>
-        /// Converts IEnumerable to IAsyncEnumerable 
-        /// </summary>
-        /// <param name="enumerable">The instance of IEnumerable to convert</param>
-        /// <param name="runSynchronously">If True the enumeration will be performed on the same thread, otherwise the MoveNext will be executed on a separate thread with Task.Run method</param>
-        /// <returns>Returns an instance of IAsyncEnumerable implementation</returns>
-        public static IAsyncEnumerable ToAsyncEnumerable(this IEnumerable enumerable, bool runSynchronously = false)
-        {
-            if (enumerable == null)
-                throw new ArgumentNullException(nameof(enumerable));
-            return enumerable as IAsyncEnumerable ?? new AsyncEnumerableWrapper<object>(enumerable.Cast<object>(), runSynchronously);
         }
     }
 }
