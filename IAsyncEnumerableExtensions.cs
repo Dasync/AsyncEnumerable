5083c6159f989c5d3ad701b8e1d0489d6d759f1c
Serge Semenov <sergii.semenov@skykick.com>
2016-04-05 12:30:00 -0700

    Add ToListAsync and ToArrayAsync extension methods

---
+++ IAsyncEnumerableExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace System.Collections.Async
+{
+    /// <summary>
+    /// Extension methods for <see cref="IAsyncEnumerable"/> interface
+    /// </summary>
+    public static class IAsyncEnumerableExtensions
+    {
+        /// <summary>
+        /// Creates a list of elements asynchronously from the enumerable source
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of source</typeparam>
+        /// <param name="source">The collection of elements</param>
+        /// <param name="cancellationToken">A cancellation token to cancel the async operation</param>
+        public static async Task<List<T>> ToListAsync<T>(this IAsyncEnumerable<T> source, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var resultList = new List<T>();
+            using (var enumerator = await source.GetAsyncEnumeratorAsync(cancellationToken)) {
+                while (await enumerator.MoveNextAsync(cancellationToken)) {
+                    resultList.Add(enumerator.Current);
+                }
+            }
+            return resultList;
+        }
+
+        /// <summary>
+        /// Creates an array of elements asynchronously from the enumerable source
+        /// </summary>
+        /// <typeparam name="T">The type of the elements of source</typeparam>
+        /// <param name="source">The collection of elements</param>
+        /// <param name="cancellationToken">A cancellation token to cancel the async operation</param>
+        public static async Task<T[]> ToArrayAsync<T>(this IAsyncEnumerable<T> source, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var list = await source.ToListAsync(cancellationToken);
+            return list.ToArray();
+        }
+    }
+}
