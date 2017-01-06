0e5621c6638f1afcbcc1a7bc60f47c5b91670ef2
Serge Semenov <sergii.semenov@skyKick.com>
2017-01-06 14:21:30 -0800

    Update comments to the AsyncEnumerationCanceledException

--- AsyncEnumerator.cs
+++ AsyncEnumerator.cs
@@ -10,6 +10,7 @@ namespace System.Collections.Async
     {
         /// <summary>
         /// This exception is thrown when you call <see cref="Yield.Break"/>
+        /// or when the enumerator is disposed before reaching the end of enumeration.
         /// </summary>
         public sealed class AsyncEnumerationCanceledException : OperationCanceledException { }
 
